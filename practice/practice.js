029ff8238985930989815dd666e87936bf4146fc
KNDahite <kalpeshdahite009@gmail.com>
2026-07-07 18:38:10 +0530

    Changes in README and added practice folder

---
+++ practice/practice.js
@@ -0,0 +1,10 @@
+console.log('Hello Java Script \nKalpesh 21 Nashik');
+console.warn('This is a warning message');
+console.error('This is an error message');
+console.table([10, 20, 30, 40, 50]);
+
+
+var studentName = 'Kalpesh Dahite'
+var age = 21
+
+console.log(studentName, age);
